c377667908a895b8ac9cdd3d0ada6950c48dea6d
asad1996172 <asad007mahmood@gmail.com>
2021-03-29 00:31:57 -0500

    Started adding Unit tests for client

--- client/app/tests/test_app.py
+++ client/app/tests/test_app.py
@@ -1,4 +1,40 @@
-import client.app
+import json
+import flask
+def test_home_page(app, client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Disaster Assistance Management System" in response.data
 
-def test_dummy():
-    assert 200 == 200
+
+def test_register_page(app, client):
+    
+    # Testing GET request
+    response = client.get('/register', follow_redirects=True)
+    assert response.status_code == 200
+    assert b"DAMS Register" in response.data
+
+    # Testing POST request
+    data = {
+        'email': 'admin@admin.com',
+        'password': '123456',
+        'name': 'Admin',
+        'options': 'admin',
+        'zipcode': '52242'
+    }
+    headers = {
+        "Content-Type": "application/form-urlencoded"
+    }
+    response = client.post(
+        '/register', data=data, headers=headers, follow_redirects=True)
+    assert response.status_code == 200
+
+
+def test_login_page(app, client):
+
+    # Testing GET request
+    client.set_cookie('localhost', 'JWT', '')
+    response = client.get('/login', follow_redirects=True)
+    assert response.status_code == 200
+    assert b"DAMS Login" in response.data
+
+    # Testing POST request
